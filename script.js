33415a041da9d7587ed58ceea4ba83b93246deaf
Cham3l30n <72985334+Cham3l30n@users.noreply.github.com>
2020-12-30 20:12:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+$buttonwhite: #aaaaaa
+$bg: #0BAAD4
+$text: rgba(255,255,255,1)
+$buttshadow: #454545
+$white: white
+$buttgreen: #4FA86B
+
+
+
+#rick
+  visibility: hidden
+    top: 49.9%
+    /* Left 49.9
+    left: 49.9%
+    /* This makes stuff centered
+    /* For Chrome
+    -webkit-transform: translate(-50.1%, -50.1%)
+    /* For ... IE
+    transform: translate(-50.1%, -50.1%)
+  position: fixed
+
+#roll
+  position: relative
+  border: none
+  outline: 0
+  border-bottom: 2px solid transparent
